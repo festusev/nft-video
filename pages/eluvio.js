a579466e93a6ad01ad1ae5a1f4866798b13b5d38
Evan Ellis <evandellis@gmail.com>
2021-10-23 11:15:54 -0700

    Added in upload button, working on a simple upload and display functionality

--- pages/eluvio.js
+++ pages/eluvio.js
@@ -4,64 +4,64 @@
 // Code for streaming video from the fabric: https://github.com/eluv-io/elv-stream-sample
 
 // From source
-import { ElvClient } from "elv-client-js";
-const { ElvClient } = require("elv-client-js");
+import { ElvClient } from "./elv-client-js/src/ElvClient";
+
+export default async function uploadToEluvio(arrayBuffer) {
+    // NOTE: THIS IS THE DEMO NET, NOT the main net
+    const client = await ElvClient.FromConfigurationUrl({
+        configUrl: "https://demov3.net955310.contentfabric.io/config"
+    });
 
 
-// NOTE: THIS IS THE DEMO NET, NOT the main net
-const client = await ElvClient.FromConfigurationUrl({
-    configUrl: "https://demov3.net955310.contentfabric.io/config"
-  });
+    // About the code below:
+    // Note: Always treat private keys (and mnemonics) as private, sensitive user data. 
+    // Always store and transfer them encrypted (the client has a method for encrypting private keys with a password - 
+    // see ElvWallet#GenerateEncryptedPrivateKey). A plaintext private key or mnemonic should never leave the user's browser - 
+    // all use of the private key is done on the client.
+    const wallet = client.GenerateWallet();
+    const signer = wallet.AddAccount({
+        privateKey: "0x0000000000000000000000000000000000000000000000000000000000000000"
+    });
+
+    client.SetSigner({signer});
 
 
-// About the code below:
-// Note: Always treat private keys (and mnemonics) as private, sensitive user data. 
-// Always store and transfer them encrypted (the client has a method for encrypting private keys with a password - 
-// see ElvWallet#GenerateEncryptedPrivateKey). A plaintext private key or mnemonic should never leave the user's browser - 
-// all use of the private key is done on the client.
-const wallet = client.GenerateWallet();
-const signer = wallet.AddAccount({
-    privateKey: "0x0000000000000000000000000000000000000000000000000000000000000000"
-});
+    // Creating content code:
+    const createResponse = await client.CreateContentObject({libraryId});
+    const objectId = createResponse.id;
+    const writeToken = createResponse.write_token;
 
-client.SetSigner({signer});
-
-
-// Creating content code:
-const createResponse = await client.CreateContentObject({libraryId});
-const objectId = createResponse.id;
-const writeToken = createResponse.write_token;
-
-await client.ReplaceMetadata({
-  libraryId,
-  objectId,
-  writeToken,
-  metadata: {
-    tags: [
-      "video",
-      "audio"
-    ]
-  }
-});
-
-await client.UploadFiles({
-  libraryId,
-  objectId,
-  writeToken,
-  fileInfo: [
-    {
-      path: "image.jpg",
-      mime_type: "image/jpeg",
-      size: 10000,
-      data: // (<ArrayBuffer 10000>)
+    await client.ReplaceMetadata({
+    libraryId,
+    objectId,
+    writeToken,
+    metadata: {
+        tags: [
+        "video",
+        "audio"
+        ]
     }
-  ]
-});
+    });
 
-const finalizeResponse = await client.FinalizeContentObject({
-  libraryId,
-  objectId,
-  writeToken
-});
+    await client.UploadFiles({
+    libraryId,
+    objectId,
+    writeToken,
+    fileInfo: [
+        {
+        path: "image.jpg",
+        mime_type: "image/jpeg",
+        size: 10000,
+        data: arrayBuffer
+        }
+    ]
+    });
 
-const versionHash = finalizeResponse.hash;
+    const finalizeResponse = await client.FinalizeContentObject({
+    libraryId,
+    objectId,
+    writeToken
+    });
+
+    const versionHash = finalizeResponse.hash;
+}
