7d445ac058d5b5763c0c7d0c6ee426f04f5cec4a
Evan Ellis <evandellis@gmail.com>
2021-10-24 08:50:46 -0700

    Fixed a lot

---
+++ examples/upload.js
@@ -0,0 +1,88 @@
+function uploadFile() {
+    console.log("uploading file");
+    let file = document.getElementById("uploadVid").files[0];
+    let reader = new FileReader();
+  
+    reader.onload = function(e) {
+        let arrayBuffer = new Uint8Array(reader.result);
+        console.log(arrayBuffer);
+        uploadToEluvio(arrayBuffer);
+    }
+  
+    reader.readAsArrayBuffer(file)
+    
+}
+
+async function uploadToEluvio(arrayBuffer) {
+    // NOTE: THIS IS THE DEMO NET, NOT the main net
+    const client = await ElvClient.FromConfigurationUrl({
+        configUrl: "https://demov3.net955210.contentfabric.io/config"
+    });
+
+
+    // About the code below:
+    // Note: Always treat private keys (and mnemonics) as private, sensitive user data. 
+    // Always store and transfer them encrypted (the client has a method for encrypting private keys with a password - 
+    // see ElvWallet#GenerateEncryptedPrivateKey). A plaintext private key or mnemonic should never leave the user's browser - 
+    // all use of the private key is done on the client.
+    const wallet = client.GenerateWallet();
+    const signer = wallet.AddAccount({
+        privateKey: "0x8145fe8c6bbf6e058fc2183ad8c6d4942a3d41ea5920b260dbf5beea06e3041b"
+    });
+
+    client.SetSigner({signer});
+
+    console.log("Library id");
+    let libraryId = "ilibVtQyvSaiKqyUjfPArSyhSe2Q51S"
+
+    // Creating content code:
+    const createResponse = await client.CreateContentObject({libraryId});
+    const objectId = createResponse.id;
+    const writeToken = createResponse.write_token;
+
+    console.log("Created response");
+
+    await client.ReplaceMetadata({
+    libraryId,
+    objectId,
+    writeToken,
+    metadata: {
+        tags: [
+        "video",
+        "audio"
+        ]
+    }
+    });
+    
+    console.log("Replaced metadata");
+
+    await client.UploadFiles({
+    libraryId,
+    objectId,
+    writeToken,
+    fileInfo: [
+        {
+        path: "video.mp4",
+        mime_type: "video/mp4",
+        size: arrayBuffer.length,
+        data: arrayBuffer
+        }
+    ]
+    });
+    console.log("Uploaded file");
+
+    const finalizeResponse = await client.FinalizeContentObject({
+    libraryId,
+    objectId,
+    writeToken
+    });
+
+    const versionHash = finalizeResponse.hash;
+
+    console.log("Version hash:")
+    console.log(versionHash);
+
+    document.getElementById("content-id").value = versionHash;
+
+    Load();
+}
